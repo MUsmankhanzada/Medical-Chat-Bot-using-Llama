7996edd54efbcc80e904ac3897b18c8c115e4554
MUsmanKhanzada <usmankz.khan363@gmail.com>
2025-07-14 17:45:08 +0200

    setup file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot Project',
+    version= '0.0.0',
+    author= 'Muhammad Usman Khanzada',
+    author_email= 'usmankz.khan363@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
