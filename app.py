b46a18aff20864ba291d414da74153c482602cfd
MUsmanKhanzada <usmankz.khan363@gmail.com>
2025-07-14 20:00:01 +0200

    app setup

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, jsonify, request
-from src.helper import download_huggingface_embedings
-from langchain.vectorstores import Pinecone
-import pinecone
+from src.helper import download_huggingface_embeddings
+from langchain_pinecone import PineconeVectorStore
+from pinecone import Pinecone
 from langchain.prompts import PromptTemplate
 from langchain_community.llms import LlamaCpp
 from langchain.chains import RetrievalQA
@@ -15,12 +15,13 @@ load_dotenv()
 
 PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
 
-embeddings = download_hugging_face_embedings()
+embeddings = download_huggingface_embeddings()
 pc = Pinecone(api_key=PINECONE_API_KEY)
 index = pc.Index("medical-bot")
+
 vector_store = PineconeVectorStore(index=index, embedding=embeddings)
 
-model_path = r".\models\tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf"
+model_path = r".\experiment\models\tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf"
 
 langchain_llm = LlamaCpp(
     model_path=model_path,  # Path to quantized GGUF model (e.g., llama-2-7b-chat.Q4_K_M.gguf)
